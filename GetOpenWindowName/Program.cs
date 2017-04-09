eaa46b726690913bfd2f4c1c408d72547d38885a
Sam Weaver <sam@samweaver.com>
2017-04-08 23:14:36 -0400

    Initial Commit v.0.0.1

---
+++ GetOpenWindowName/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetOpenWindowName
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Process[] processList = Process.GetProcesses();
+            SortedSet<Process> sortedProcessList = new SortedSet<Process>(new ProcessComparer());
+
+            foreach (Process process in processList)
+            {
+                if (!String.IsNullOrEmpty(process.MainWindowTitle))
+                {
+                    sortedProcessList.Add(process);
+                }
+            }
+
+            foreach (Process process in sortedProcessList)
+            {
+                int z = ProcessComparer.GetZOrder(process);
+                //Console.WriteLine("({0}) Process: {1} ID: {2} Window title: {3}", z, process.ProcessName, process.Id, process.MainWindowTitle);
+                Console.WriteLine("{0}", process.MainWindowTitle);
+            }
+
+            //Console.ReadLine();
+        }
+
+
+    }
+
+    class ProcessComparer : IComparer<Process>
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr GetWindow(IntPtr hWnd, int nIndex);
+        private enum GetWindowType : uint
+        {
+            /// <summary>
+            /// The retrieved handle identifies the window of the same type that is highest in the Z order.
+            /// <para/>
+            /// If the specified window is a topmost window, the handle identifies a topmost window.
+            /// If the specified window is a top-level window, the handle identifies a top-level window.
+            /// If the specified window is a child window, the handle identifies a sibling window.
+            /// </summary>
+            GW_HWNDFIRST = 0,
+            /// <summary>
+            /// The retrieved handle identifies the window of the same type that is lowest in the Z order.
+            /// <para />
+            /// If the specified window is a topmost window, the handle identifies a topmost window.
+            /// If the specified window is a top-level window, the handle identifies a top-level window.
+            /// If the specified window is a child window, the handle identifies a sibling window.
+            /// </summary>
+            GW_HWNDLAST = 1,
+            /// <summary>
+            /// The retrieved handle identifies the window below the specified window in the Z order.
+            /// <para />
+            /// If the specified window is a topmost window, the handle identifies a topmost window.
+            /// If the specified window is a top-level window, the handle identifies a top-level window.
+            /// If the specified window is a child window, the handle identifies a sibling window.
+            /// </summary>
+            GW_HWNDNEXT = 2,
+            /// <summary>
+            /// The retrieved handle identifies the window above the specified window in the Z order.
+            /// <para />
+            /// If the specified window is a topmost window, the handle identifies a topmost window.
+            /// If the specified window is a top-level window, the handle identifies a top-level window.
+            /// If the specified window is a child window, the handle identifies a sibling window.
+            /// </summary>
+            GW_HWNDPREV = 3,
+            /// <summary>
+            /// The retrieved handle identifies the specified window's owner window, if any.
+            /// </summary>
+            GW_OWNER = 4,
+            /// <summary>
+            /// The retrieved handle identifies the child window at the top of the Z order,
+            /// if the specified window is a parent window; otherwise, the retrieved handle is NULL.
+            /// The function examines only child windows of the specified window. It does not examine descendant windows.
+            /// </summary>
+            GW_CHILD = 5,
+            /// <summary>
+            /// The retrieved handle identifies the enabled popup window owned by the specified window (the
+            /// search uses the first such window found using GW_HWNDNEXT); otherwise, if there are no enabled
+            /// popup windows, the retrieved handle is that of the specified window.
+            /// </summary>
+            GW_ENABLEDPOPUP = 6
+        }
+
+        public int Compare(Process x, Process y)
+        {
+            int zOrderX = GetZOrder(x);
+            int zOrderY = GetZOrder(y);
+
+            return zOrderX - zOrderY;
+        }
+
+        public static int GetZOrder(Process p)
+        {
+            IntPtr hWnd = p.MainWindowHandle;
+            var z = 0;
+            // 3 is GetWindowType.GW_HWNDPREV
+            for (var h = hWnd; h != IntPtr.Zero; h = GetWindow(h, 3)) z++;
+            return z;
+        }
+    }
+}
